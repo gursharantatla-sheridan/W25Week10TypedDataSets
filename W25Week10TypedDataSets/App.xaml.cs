bd0e5071b9ce6ecc206eb513fc34b9f9b8cb6bd8
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-03-19 10:19:57 -0400

    Add project files.

---
+++ W25Week10TypedDataSets/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace W25Week10TypedDataSets;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
